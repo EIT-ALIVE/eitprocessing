a5bcf74ba6b743b64400a86dc75d940c019bfe25
DaniBodor <d.bodor@esciencecenter.nl>
2023-05-22 09:14:20 +0200

    recover init imports

--- eitprocessing/binreader/__init__.py
+++ eitprocessing/binreader/__init__.py
@@ -1,3 +1,12 @@
 """Loading and processing binary EIT data from the Dräger Pulmovista 500"""
 
 __version__ = "0.1"
+
+from .frameset import Frameset
+from .phases import MaxValue
+from .phases import MinValue
+from .reader import Reader
+from .sequence import Sequence
+
+
+__all__ = ['Frameset', 'MaxValue', 'MinValue', 'Reader',  'Sequence', ]
