1bc22de4d7292367321c0430e681854390080619
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2023-06-10 11:22:55 +0200

    sys path change

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import subprocess
 import sys
 
 
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # -- Project information -----------------------------------------------------
 
