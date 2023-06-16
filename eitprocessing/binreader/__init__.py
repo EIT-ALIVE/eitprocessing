0f313d64448940ec0ccb8f825e80e6392f2ce503
Peter Somhorst <peter.somhorst@gmail.com>
2023-06-16 12:32:22 +0200

    Add DraegerSequence and TimpelSequence to __init__

--- eitprocessing/binreader/__init__.py
+++ eitprocessing/binreader/__init__.py
@@ -6,8 +6,19 @@ from .frameset import Frameset
 from .phases import MaxValue
 from .phases import MinValue
 from .phases import QRSMark
+from .sequence import DraegerSequence
 from .sequence import Sequence
+from .sequence import TimpelSequence
 from .sequence import Vendor
 
 
-__all__ = ['Frameset', 'MaxValue', 'MinValue', 'QRSMark',  'Sequence', 'Vendor']
+__all__ = [
+    "Frameset",
+    "MaxValue",
+    "MinValue",
+    "QRSMark",
+    "Sequence",
+    "DraegerSequence",
+    "TimpelSequence",
+    "Vendor",
+]
