f578efd2d37878839477b0a71f0b6aab7f13af93
Peter Somhorst <p.somhorst@erasmusmc.nl>
2023-09-04 10:19:36 +0200

    Add attribute checks when initializing a ButterworthFilter

--- eitprocessing/filters/butterworth_filters.py
+++ eitprocessing/filters/butterworth_filters.py
@@ -2,10 +2,15 @@ from scipy import signal
 from . import TimeDomainFilter
 
 
+MIN_ORDER = 1
+MAX_ORDER = 10
+FILTER_TYPES = ["lowpass", "highpass", "bandpass", "bandstop"]
+
+
 class ButterworthFilter(TimeDomainFilter):
     """Butterworth Filter"""
 
-    def __init__(
+    def __init__(  # pylint: disable=too-many-arguments
         self,
         filter_type: str,
         cutoff_frequency: float | tuple[float, float],
@@ -13,17 +18,50 @@ class ButterworthFilter(TimeDomainFilter):
         sample_frequency: float,
         override_order: bool = False,
     ):
+        if filter_type not in FILTER_TYPES:
+            raise ValueError(
+                f"The filter type should be one of {', '.join(FILTER_TYPES)}, not {filter_type}."
+            )
+        self.filter_type = filter_type
+
+        if filter_type in ("lowpass", "highpass"):
+            if not isinstance(cutoff_frequency, (int, float)):
+                raise TypeError("`cutoff_frequency` should be an integer or float")
+
+        elif filter_type in ("bandpass", "bandstop"):
+            if not isinstance(cutoff_frequency, tuple):
+                if isinstance(cutoff_frequency, str):
+                    raise TypeError("`cutoff_frequency` should be a tuple")
+                try:
+                    cutoff_frequency = tuple(cutoff_frequency)
+                except TypeError as e:
+                    raise TypeError(
+                        "`cutoff_frequency` should be (castable to) a tuple"
+                    ) from e
+
+            if len(cutoff_frequency) != 2:
+                raise ValueError("`cutoff_frequency` should have length 2")
+
+            if not all(isinstance(value, (int, float)) for value in cutoff_frequency):
+                raise TypeError(
+                    "`cutoff_frequency` should be a tuple containing two numbers"
+                )
+        
         self.cutoff_frequency = cutoff_frequency
 
-        if order < 1 or (order > 10 and override_order is False):
+        if order < MIN_ORDER or (order > MAX_ORDER and override_order is False):
             raise AttributeError(
-                "Order should be an integer between 1 and 10. "
+                f"Order should be between {MIN_ORDER} and {MAX_ORDER}. "
                 + "To use higher values, set `override_order` to `True`."
             )
-
         self.order = order
+
+        if not isinstance(sample_frequency, (int, float)):
+            raise TypeError('`sample_frequency` should be a positive number')
+        if sample_frequency <= 0:
+            raise ValueError('`sample_frequency` should be positive')
         self.sample_frequency = sample_frequency
-        self.filter_type = filter_type
+
 
     def __eq__(self, other):
         if not isinstance(other, ButterworthFilter):
