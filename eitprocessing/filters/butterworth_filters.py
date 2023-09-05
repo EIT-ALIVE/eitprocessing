69dc4ea66475a1c9c339d2b8bad9fb157bde5f75
Peter Somhorst <p.somhorst@erasmusmc.nl>
2023-09-05 16:18:47 +0200

    Add documentation for all classes and methods

--- eitprocessing/filters/butterworth_filters.py
+++ eitprocessing/filters/butterworth_filters.py
@@ -11,7 +11,28 @@ FILTER_TYPES = ["lowpass", "highpass", "bandpass", "bandstop"]
 
 
 class ButterworthFilter(TimeDomainFilter):
-    """Butterworth Filter"""
+    """Butterworth filter for filtering in the time domain.
+
+    Generates a low-pass, high-pass, band-pass or band-stop digital Butterworth filter of order
+    `order`.
+
+    ``ButterworthFilter`` is a wrapper of the `scipy.butter()` and `scipy.filtfilt()` functions.
+
+    Args:
+        filter_type: The type of filter to create.
+        cutoff_frequency: Single frequency (lowpass or highpass filter) or tuple containing two
+            frequencies (bandpass and bandstop filters).
+        order: Filter order.
+        sample_frequency: Sample frequency of the data to be filtered.
+        override_order: Whether to raise an exception if the order is larger than the maximum of
+            10. Defaults to False.
+
+    Examples:
+        >>> t = np.arange(0, 100, 0.1)
+        >>> signal = np.sin(t) + 0.1 * np.sin(10 * t)
+        >>> lowpass_filter = ButterworthFilter('lowpass', 45, 4, 250)
+        >>> filtered_signal = lowpass_filter.apply_filter(signal)
+    """
 
     def __init__(  # pylint: disable=too-many-arguments
         self,
@@ -23,7 +44,8 @@ class ButterworthFilter(TimeDomainFilter):
     ):
         if filter_type not in FILTER_TYPES:
             raise ValueError(
-                f"The filter type should be one of {', '.join(FILTER_TYPES)}, not {filter_type}."
+                "The filter type should be one of "
+                f"{', '.join(FILTER_TYPES)}, not '{filter_type}'."
             )
         self.filter_type = filter_type
 
@@ -42,28 +64,38 @@ class ButterworthFilter(TimeDomainFilter):
                 raise TypeError(
                     "`cutoff_frequency` should be a tuple containing two numbers"
                 )
-        
+
         self.cutoff_frequency = cutoff_frequency
 
         if order < MIN_ORDER or (order > MAX_ORDER and override_order is False):
             raise ValueError(
                 f"Order should be between {MIN_ORDER} and {MAX_ORDER}. "
-                + "To use higher values, set `override_order` to `True`."
+                "To use higher values, set `override_order` to `True`."
             )
         self.order = order
 
         if not isinstance(sample_frequency, (int, float)):
-            raise TypeError('`sample_frequency` should be a number')
+            raise TypeError("`sample_frequency` should be a number")
         if sample_frequency <= 0:
-            raise ValueError('`sample_frequency` should be positive')
+            raise ValueError("`sample_frequency` should be positive")
         self.sample_frequency = sample_frequency
 
     def __eq__(self, other: "ButterworthFilter"):
+        """Return True if other is a ``ButterworthFilter``, and attributes match."""
         if not isinstance(other, ButterworthFilter):
             return False
         return self.__dict__ == other.__dict__
 
     def apply_filter(self, input_data: npt.ArrayLike) -> np.ndarray:
+        """Apply the filter to the input data.
+
+        Args:
+            input_data: Data to be filtered. If the input data has more than one axis,
+                the filter is applied to the last axis.
+
+        Returns:
+            The filtered output with the same shape as the input data.
+        """
         b, a = signal.butter(
             N=self.order,
             Wn=self.cutoff_frequency,
@@ -78,6 +110,8 @@ class ButterworthFilter(TimeDomainFilter):
 
 
 class SpecifiedButterworthFilter(ButterworthFilter):
+    """Superclass of specified convenience classes based on ``ButterworthFilter``."""
+
     available_in_gui = False
 
     def __init__(self, *args, **kwargs):
@@ -88,20 +122,44 @@ class SpecifiedButterworthFilter(ButterworthFilter):
 
 
 class LowPassFilter(SpecifiedButterworthFilter):
+    """Low-pass Butterworth filter for filtering in the time domain.
+
+    ``LowPassFilter`` is a convenience class similar to ``ButterworthFilter``, where the
+    `filter_type` is set to "lowpass".
+    """
+
     available_in_gui = True
     filter_type = "lowpass"
 
 
 class HighPassFilter(SpecifiedButterworthFilter):
+    """High-pass Butterworth filter for filtering in the time domain.
+
+    ``HighPassFilter`` is a convenience class similar to ``ButterworthFilter``, where the
+    `filter_type` is set to "highpass".
+    """
+
     available_in_gui = True
     filter_type = "highpass"
 
 
 class BandStopFilter(SpecifiedButterworthFilter):
+    """Band-stop Butterworth filter for filtering in the time domain.
+
+    ``BandStopFilter`` is a convenience class similar to ``ButterworthFilter``, where the
+    `filter_type` is set to "bandstop".
+    """
+
     available_in_gui = True
     filter_type = "bandstop"
 
 
 class BandPassFilter(SpecifiedButterworthFilter):
+    """Band-pass Butterworth filter for filtering in the time domain.
+
+    ``BandPassFilter`` is a convenience class similar to ``ButterworthFilter``, where the
+    `filter_type` is set to "bandpass".
+    """
+
     available_in_gui = True
     filter_type = "bandpass"
