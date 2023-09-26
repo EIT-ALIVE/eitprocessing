42599861c71e3a9154ef98c19659ab183e35076d
Peter Somhorst <p.somhorst@erasmusmc.nl>
2023-09-26 10:05:27 +0200

    Extend, update and improve documentation

--- eitprocessing/roi_selection/gridselection.py
+++ eitprocessing/roi_selection/gridselection.py
@@ -12,12 +12,33 @@ from . import ROISelection
 class GridSelection(ROISelection):
     """Create regions of interest by division into a grid.
 
-    GridSelection allows for the creation a list of matrices that can be used to divide a
-    two-dimensional array into several groups structured in a grid. An instance of GridSelection
-    contains information about how to subdivide an input matrix. Calling `find_grid(data)` results
-    in a list of matrices with the same dimension as `data`, each representing a single group. A
-    matrix contains the value False or 0 for pixels that do not belong to the group, and the value
-    True, 1 or any number between 0 and 1 for pixels that (partly) belong to the group.
+    GridSelection allows for the creation a list of 2D arrays that can be used to divide a two- or
+    higher-dimensional array into several regions structured in a grid. An instance of
+    GridSelection contains information about how to subdivide an input matrix. Calling
+    `find_grid(data)`, where data is a 2D array, results in a list of arrays with the same
+    dimension as `data`, each representing a single region. Each resulting 2D array contains the
+    value False or 0 for pixels that do not belong to the region, and the value True, 1 or any
+    number between 0 and 1 for pixels that (partly) belong to the region.
+
+    Rows and columns at the edges of `data` that only contain NaN (not a number) values are
+    ignored. E.g. a (32, 32) array where the first and last two rows and first and last two columns
+    only contain NaN are split as if it is a (28, 28) array. The resulting arrays have the shape
+    (32, 32) with the same rows and columns only containing NaN values.
+
+    If the number of rows or columns can not split evenly, a row or column can be split among two
+    regions. This behaviour is controlled by `split_pixels`.
+
+    If `split_pixels` is `True`, e.g. a (2, 5) array that is split in two horizontal regions, the
+    first region will contain the first two columns, and half of the third column. The second
+    region contains half of the third columns, and the last column.
+
+    If `split_pixels` is `False` (default), rows and columns will not be split. A warning will be
+    shown stating regions don't contain equal numbers of rows/columns. The regions towards the top
+    and left will be larger. E.g., when a (2, 5) array is split in two horizontal regions, the
+    first region will contain the first three columns, and the second region the last two columns.
+
+    Regions are ordered according to C indexing order. The `matrix_layout()` method provides a map
+    showing how the regions are ordered.
 
     Common grids are pre-defined:
     - VentralAndDorsal: vertically divided into ventral and dorsal;
@@ -27,20 +48,42 @@ class GridSelection(ROISelection):
     - Quadrants: vertically and horizontally divided into four quadrants.
 
     Args:
-        v_split: The number of vertical groups. Must be 1 or larger.
-        h_split: The number of horizontal groups. Must be 1 or larger.
-        split_pixels: Allows rows and columns to be split over two groups. Currently not
-            implemented.
+        v_split: The number of vertical regions. Must be 1 or larger.
+        h_split: The number of horizontal regions. Must be 1 or larger.
+        split_pixels: Allows rows and columns to be split over two regions.
 
-    Example:
-    >>> gs = GridSelection(3, 1)
-    >>> matrices = gs.find_grid(included_pixels)
-    >>> sum_group1 = np.sum(matrices[0] * pixel_amplitude)
-    >>> print(gs.matrix_layout())
-    array([[0],
-           [1],
-           [2]])
-
+    Examples:
+        >>> pixel_map = array([[ 1,  2,  3],
+                               [ 4,  5,  6],
+                               [ 7,  8,  9],
+                               [10, 11, 12],
+                               [13, 14, 15],
+                               [16, 17, 18]])
+        >>> gs = GridSelection(3, 1, split_pixels=False)
+        >>> matrices = gs.find_grid(pixel_map)
+        >>> matrices[0] * pixel_map
+        array([[1, 2, 3],
+               [4, 5, 6],
+               [0, 0, 0],
+               [0, 0, 0],
+               [0, 0, 0],
+               [0, 0, 0]])
+        >>> gs.matrix_layout()
+        array([[0],
+               [1],
+               [2]])
+        >>> gs2 = GridSelection(2, 2, split_pixels=True)
+        >>> matrices2 = gs.find_grid(pixel_map)
+        >>> gs2.matrix_layout()
+        array([[0, 1],
+               [2, 3]])
+        >>> matrices2[2]
+        array([[0. , 0. , 0. ],
+               [0. , 0. , 0. ],
+               [0. , 0. , 0. ],
+               [1. , 0.5, 0. ],
+               [1. , 0.5, 0. ],
+               [1. , 0.5, 0. ]])
     """
 
     v_split: int
@@ -112,7 +155,8 @@ class GridSelection(ROISelection):
 
             if n_vectors_per_region % 1 > 0:
                 warnings.warn(
-                    f"The {orientation} groups will not have an equal number of {'columns' if horizontal else 'rows'}. "
+                    f"The {orientation} regions will not have an equal number of "
+                    f"{'columns' if horizontal else 'rows'}. "
                     f"{n_vectors} is not equally divisible by {n_regions}.",
                     RuntimeWarning,
                 )
@@ -206,8 +250,8 @@ class GridSelection(ROISelection):
 
     def matrix_layout(self):
         """Returns an array showing the layout of the matrices returned by `find_grid`."""
-        n_groups = self.v_split * self.h_split
-        return np.reshape(np.arange(n_groups), (self.v_split, self.h_split))
+        n_regions = self.v_split * self.h_split
+        return np.reshape(np.arange(n_regions), (self.v_split, self.h_split))
 
 
 class InvalidDivision(Exception):
@@ -224,6 +268,8 @@ class InvalidVerticalDivision(InvalidDivision):
 
 @dataclass
 class VentralAndDorsal(GridSelection):
+    """Split data into a ventral and dorsal region of interest."""
+
     v_split: Literal[2] = field(default=2, init=False)
     h_split: Literal[1] = field(default=1, init=False)
     split_pixels: bool = False
@@ -231,6 +277,8 @@ class VentralAndDorsal(GridSelection):
 
 @dataclass
 class RightAndLeft(GridSelection):
+    """Split data into a right and left region of interest."""
+
     v_split: Literal[1] = field(default=1, init=False)
     h_split: Literal[2] = field(default=2, init=False)
     split_pixels: bool = False
@@ -238,6 +286,8 @@ class RightAndLeft(GridSelection):
 
 @dataclass
 class FourLayers(GridSelection):
+    """Split data vertically into four layer regions of interest."""
+
     v_split: Literal[4] = field(default=4, init=False)
     h_split: Literal[1] = field(default=1, init=False)
     split_pixels: bool = False
@@ -245,6 +295,8 @@ class FourLayers(GridSelection):
 
 @dataclass
 class Quadrants(GridSelection):
+    """Split data into four quadrant regions of interest."""
+
     v_split: Literal[2] = field(default=2, init=False)
     h_split: Literal[2] = field(default=2, init=False)
     split_pixels: bool = False
