bed66d552c0e4fee14cd429ac951d7745eee8219
Peter Somhorst <p.somhorst@erasmusmc.nl>
2023-09-29 10:47:49 +0200

    Unify split pixels an non-split pixels function to reduce code reuse

--- eitprocessing/roi_selection/gridselection.py
+++ eitprocessing/roi_selection/gridselection.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 from dataclasses import field
 from typing import Literal
 import numpy as np
+from numpy.typing import NDArray
 from . import ROISelection
 
 
@@ -116,126 +117,21 @@ class GridSelection(ROISelection):
             )
 
     def find_grid(self, data) -> list:
-        n_rows, n_columns = data.shape
+        function = (
+            self._create_grouping_vector_split_pixels
+            if self.split_pixels
+            else self._create_grouping_vector_no_split_pixels
+        )
+        horizontal_grouping_vectors = function(data, "horizontal", self.h_split)
 
-        if self.split_pixels:
-            # TODO: create warning if number of regions > number of columns/rows
-            return self._find_grid_split_pixels(data)
-
-        if self.h_split > n_columns:
-            raise InvalidHorizontalDivision(
-                f"`h_split` ({self.h_split}) is larger than the number of columns ({n_columns})."
-            )
-
-        if self.v_split > n_rows:
-            raise InvalidVerticalDivision(
-                f"`v_split` ({self.v_split}) is larger than the number or rows ({n_rows})."
-            )
-
-        return self._find_grid_no_split_pixels(data)
-
-    def _find_grid_no_split_pixels(self, data):
-        # Detect upper, lower, left and right column of grid selection,
-        # i.e, find the first row and column where the sum of pixels in that column is not NaN
-
-        is_numeric = ~np.isnan(data)
-
-        def get_region_boundaries(
-            orientation: Literal["horizontal", "vertical"], n_regions: int
-        ):
-            horizontal = orientation == "horizontal"
-            axis = 0 if horizontal else 1
-            vector_has_numeric_cells = is_numeric.sum(axis) > 0
-            numeric_vector_indices = np.argwhere(vector_has_numeric_cells)
-            first_numeric_vector = numeric_vector_indices.min()
-            last_vector_numeric = numeric_vector_indices.max()
-
-            n_vectors = last_vector_numeric - first_numeric_vector + 1
-            n_vectors_per_region = n_vectors / n_regions
-
-            if n_vectors_per_region % 1 > 0:
-                warnings.warn(
-                    f"The {orientation} regions will not have an equal number of "
-                    f"{'columns' if horizontal else 'rows'}. "
-                    f"{n_vectors} is not equally divisible by {n_regions}.",
-                    RuntimeWarning,
-                )
-
-            region_boundaries = [
-                first_numeric_vector
-                + bisect.bisect_left(np.arange(n_vectors) / n_vectors_per_region, c)
-                for c in range(n_regions + 1)
-            ]
-            return region_boundaries
-
-        h_boundaries = get_region_boundaries("horizontal", n_regions=self.h_split)
-        v_boundaries = get_region_boundaries("vertical", n_regions=self.v_split)
+        function = (
+            self._create_grouping_vector_split_pixels
+            if self.split_pixels
+            else self._create_grouping_vector_no_split_pixels
+        )
+        vertical_grouping_vectors = function(data, "vertical", self.v_split)
 
         matrices = []
-        for v_start, v_end in itertools.pairwise(v_boundaries):
-            for h_start, h_end in itertools.pairwise(h_boundaries):
-                matrix = np.copy(is_numeric)
-                matrix[:, :h_start] = False
-                matrix[:, h_end:] = False
-                matrix[:v_start, :] = False
-                matrix[v_end:, :] = False
-                matrices.append(matrix)
-
-        return matrices
-
-    def _find_grid_split_pixels(self, data):
-        def create_grouping_vector(matrix, axis, n_groups):
-            """Create a grouping vector to split vector into `n` groups."""
-
-            # create a vector that is nan if the entire column/row is nan, 1 otherwise
-            nan = np.all(np.isnan(matrix), axis=axis)
-            vector = np.ones(nan.shape)
-            vector[nan] = np.nan
-
-            # remove non-numeric (nan) elements at vector ends
-            # nan elements between numeric elements are kept
-            numeric_element_indices = np.argwhere(~np.isnan(vector))
-            first_num_element = numeric_element_indices.min()
-            last_num_element = numeric_element_indices.max()
-            n_elements = last_num_element - first_num_element + 1
-
-            group_size = n_elements / n_groups
-
-            # find the right boundaries (upper values) of each group
-            right_boundaries = (np.arange(n_groups) + 1) * group_size
-            right_boundaries = right_boundaries[:, None]  # converts it to a row vector
-
-            # each row in the base represents one group
-            base = np.tile(np.arange(n_elements), (n_groups, 1))
-
-            # if the element number is higher than the split, it does not belong in this group
-            element_contribution_to_group = right_boundaries - base
-            element_contribution_to_group[element_contribution_to_group < 0] = 0
-
-            # if the element to the right is a full group size, this element is ruled out
-            rule_out = element_contribution_to_group[:, 1:] >= group_size
-            element_contribution_to_group[:, :-1][rule_out] = 0
-
-            # elements have a maximum value of 1
-            element_contribution_to_group = np.fmin(element_contribution_to_group, 1)
-
-            # if this element is already represented in the previous group (row), subtract that
-            element_contribution_to_group[1:] -= element_contribution_to_group[:-1]
-            element_contribution_to_group[element_contribution_to_group < 0] = 0
-
-            # element_contribution_to_group only represents non-nan elements
-            # insert into final including non-nan elements
-            final = np.full((n_groups, len(vector)), np.nan)
-            final[
-                :, first_num_element : last_num_element + 1
-            ] = element_contribution_to_group
-            return final
-
-        horizontal_grouping_vectors = create_grouping_vector(data, 0, self.h_split)
-        vertical_grouping_vectors = create_grouping_vector(data, 1, self.v_split)
-
-        matrices = []
-
         for vertical, horizontal in itertools.product(
             vertical_grouping_vectors, horizontal_grouping_vectors
         ):
@@ -243,12 +139,112 @@ class GridSelection(ROISelection):
             matrix[np.isnan(data)] = np.nan
             matrix *= horizontal
             matrix *= vertical[:, None]  # [:, None] converts to a column vector
-
             matrices.append(matrix)
 
         return matrices
 
-    def matrix_layout(self):
+    @staticmethod
+    def _create_grouping_vector_no_split_pixels(  # pylint: disable=too-many-locals
+        data: NDArray, orientation: Literal["horizontal", "vertical"], n_regions: int
+    ) -> list[NDArray]:
+        is_numeric = ~np.isnan(data)
+        horizontal = orientation == "horizontal"
+        axis = 0 if horizontal else 1
+        numeric_vector_indices = np.argwhere(is_numeric.sum(axis) > 0)
+        first_numeric_vector = numeric_vector_indices.min()
+        last_vector_numeric = numeric_vector_indices.max()
+
+        n_vectors = last_vector_numeric - first_numeric_vector + 1
+
+        if n_regions > n_vectors:
+            if horizontal:  # pylint: disable=no-else-raise
+                raise InvalidHorizontalDivision(
+                    f"The number horizontal regions is larger than the "
+                    f"number of available columns ({n_vectors})."
+                )
+            else:
+                raise InvalidVerticalDivision(
+                    f"The number vertical regions is larger than the "
+                    f"number of available rows ({n_vectors})."
+                )
+
+        n_vectors_per_region = n_vectors / n_regions
+
+        if n_vectors_per_region % 1 > 0:
+            warnings.warn(
+                f"The {orientation} regions will not have an equal number of "
+                f"{'columns' if horizontal else 'rows'}. "
+                f"{n_vectors} is not equally divisible by {n_regions}.",
+                RuntimeWarning,
+            )
+
+        region_boundaries = [
+            first_numeric_vector
+            + bisect.bisect_left(np.arange(n_vectors) / n_vectors_per_region, c)
+            for c in range(n_regions + 1)
+        ]
+
+        vectors = []
+        for start, end in itertools.pairwise(region_boundaries):
+            vector = np.ones(data.shape[1 - axis])
+            vector[:start] = 0
+            vector[end:] = 0
+            vectors.append(vector)
+
+        return vectors
+
+    @staticmethod
+    def _create_grouping_vector_split_pixels(  # pylint: disable=too-many-locals
+        matrix: NDArray, orientation: Literal["horizontal", "vertical"], n_groups: int
+    ) -> NDArray:
+        """Create a grouping vector to split vector into `n` groups."""
+        axis = 0 if orientation == "horizontal" else 1
+
+        # create a vector that is nan if the entire column/row is nan, 1 otherwise
+        vector_is_nan = np.all(np.isnan(matrix), axis=axis)
+        vector = np.ones(vector_is_nan.shape)
+        vector[vector_is_nan] = np.nan
+
+        # remove non-numeric (nan) elements at vector ends
+        # nan elements between numeric elements are kept
+        numeric_element_indices = np.argwhere(~np.isnan(vector))
+        first_num_element = numeric_element_indices.min()
+        last_num_element = numeric_element_indices.max()
+        n_elements = last_num_element - first_num_element + 1
+
+        group_size = n_elements / n_groups
+
+        # find the right boundaries (upper values) of each group
+        right_boundaries = (np.arange(n_groups) + 1) * group_size
+        right_boundaries = right_boundaries[:, None]  # converts it to a row vector
+
+        # each row in the base represents one group
+        base = np.tile(np.arange(n_elements), (n_groups, 1))
+
+        # if the element number is higher than the split, it does not belong in this group
+        element_contribution_to_group = right_boundaries - base
+        element_contribution_to_group[element_contribution_to_group < 0] = 0
+
+        # if the element to the right is a full group size, this element is ruled out
+        rule_out = element_contribution_to_group[:, 1:] >= group_size
+        element_contribution_to_group[:, :-1][rule_out] = 0
+
+        # elements have a maximum value of 1
+        element_contribution_to_group = np.fmin(element_contribution_to_group, 1)
+
+        # if this element is already represented in the previous group (row), subtract that
+        element_contribution_to_group[1:] -= element_contribution_to_group[:-1]
+        element_contribution_to_group[element_contribution_to_group < 0] = 0
+
+        # element_contribution_to_group only represents non-nan elements
+        # insert into final including non-nan elements
+        final = np.full((n_groups, len(vector)), np.nan)
+        final[
+            :, first_num_element : last_num_element + 1
+        ] = element_contribution_to_group
+        return final
+
+    def matrix_layout(self) -> NDArray:
         """Returns an array showing the layout of the matrices returned by `find_grid`."""
         n_regions = self.v_split * self.h_split
         return np.reshape(np.arange(n_regions), (self.v_split, self.h_split))
