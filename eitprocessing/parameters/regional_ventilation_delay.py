00ee28398032b6069bae71bd50ad22706d90754c
JulietteFrancovich <juliette.francovich@hotmail.com>
2023-11-02 10:36:15 +0100

    created file for RVD

---
+++ eitprocessing/parameters/regional_ventilation_delay.py
@@ -0,0 +1,24 @@
+import numpy as np
+from numpy.typing import NDArray
+from . import ParameterExtraction
+
+
+class RegionalVentilationDelay(ParameterExtraction):
+    def compute_parameter(self, pixel_dZ: NDArray) -> list:
+        """Calculate global inhomogeneity
+
+        Args:
+            pixel_dZ: (n, 32, 32) np.ndarray containing dZ values per pixel for n breaths.
+
+        Returns:
+            global_inhomogeneity (float): global inhomogeneity in percentage
+        """
+        global_inhomogeneity = []
+        for breath in range(pixel_dZ.shape[0]):
+            nan_mask = np.isnan(pixel_dZ[breath, :, :])
+            ventilated_area_dz = pixel_dZ[breath, ~nan_mask]
+            med_dz_lung = np.median(ventilated_area_dz)
+            abs_diff = [abs(pixel_dz - med_dz_lung) for pixel_dz in ventilated_area_dz]
+            global_inhomogeneity.append((sum(abs_diff) / sum(ventilated_area_dz)) * 100)
+
+        return global_inhomogeneity
