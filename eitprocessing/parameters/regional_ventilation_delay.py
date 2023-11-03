753917380d673763c742b285c00bbfb96a198000
JulietteFrancovich <juliette.francovich@hotmail.com>
2023-11-03 14:21:54 +0100

    added summary stats

--- eitprocessing/parameters/regional_ventilation_delay.py
+++ eitprocessing/parameters/regional_ventilation_delay.py
@@ -10,11 +10,17 @@ class RegionalVentilationDelay(ParameterExtraction):
     breath_detection_kwargs: dict = {}
     interpolation_kind = "cubic"  # Type of interpolation used for normalizing all inspirations between 0 and 1
     common_time = np.linspace(0, 1, 101)  # Common time axis to which is normalized
+    summary_stats: dict[str, Callable[[NDArray], float]] = {
+        "mean": np.mean,
+        "standard deviation": np.std,
+        "median": np.median,
+    }
 
-    def compute_parameter(self, sequence, frameset_name: str) -> tuple[NDArray, float]:
-        """Computes the regional ventilation delay per breath using pixel
+    def compute_parameter(self, sequence, frameset_name: str) -> tuple[NDArray, dict]:
+        """Computes the mean regional ventilation delay over all breaths using pixel
         impedance. Also returns the regional ventilation delay inhomogeneity
-        (standard deviation of RVD of all pixels)."""
+        (standard deviation of RVD of all pixels) in a dictionary according to
+        the required summary statistics."""
         global_impedance = sequence.framesets[frameset_name].global_impedance
         breath_detector = BreathDetection(
             sequence.framerate, **self.breath_detection_kwargs
@@ -23,7 +29,8 @@ class RegionalVentilationDelay(ParameterExtraction):
 
         pixel_impedance = sequence.framesets[frameset_name].pixel_values
 
-        interpolated_inspirations = []
+        regional_ventilation_delays = []
+        rvd_inhomogeneities = []
         for breath in breaths:
             num_timepoints = breath.middle_index - breath.start_index
 
@@ -45,24 +52,18 @@ class RegionalVentilationDelay(ParameterExtraction):
             )
             interpolated_inspirations.append(interpolated_inspiration)
 
-        # Mean inspiration for each pixel
-        mean_pixel_inspiration = np.mean(interpolated_inspirations, axis=0)
+            offset = interpolated_inspirations[0, :, :]
+            interpolated_inspirations -= offset
+            max_impedance = interpolated_inspirations.max(axis=0)
+            normalized_pixel_tiv = (interpolated_inspirations - 0) / (max_impedance - 0)
+            rvd = np.nanargmax(normalized_pixel_tiv > 0.4, axis=0)
+            regional_ventilation_delays.append(rvd)
+            rvd_inhomogeneities.append(np.std(rvd))
 
-        offset = mean_pixel_inspiration[0, :, :]
-        mean_pixel_inspiration -= offset
-        max_impedance = mean_pixel_inspiration.max(axis=0)
-        normalized_pixel_tiv = (mean_pixel_inspiration - 0) / (max_impedance - 0)
-        rvd = np.nanargmax(normalized_pixel_tiv > 0.4, axis=0)
-        rvdi = np.std(rvd)
+        mean_rvd = np.mean(regional_ventilation_delays, axis=0)
 
-        return (rvd, rvdi)
+        rvd_inhomogeneity = {}
+        for name, function in self.summary_stats.items():
+            rvd_inhomogeneity[name] = function(rvd_inhomogeneities)
 
-    # for row in range(0,32):
-    #     for col in range (0,32):
-    #         if isinstance(mean_pixel_inspiration[row,col], np.ndarray):
-    #             plt.plot(COMMON_TIME, normalized_pixel_tiv[:,row,col])
-    #             plt.title(f'Normalized pixel impedance of all ventilated pixels at {timepoint}')
-    #             plt.xlabel('Normalized time')
-    #             plt.ylabel('Normalized impedance')
-    # plt.savefig(results_path / f'Normalized pixel impedance of all ventilated pixels - patient {patient_ID} - {timepoint}.png', dpi=300)
-    # plt.close()
+        return (mean_rvd, rvd_inhomogeneity)
