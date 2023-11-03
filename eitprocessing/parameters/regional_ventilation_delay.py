4527446f0842d2e91d402fe12b5fa4a9930d03fd
JulietteFrancovich <juliette.francovich@hotmail.com>
2023-11-03 11:30:23 +0100

    added time normalization to pixel impedance

--- eitprocessing/parameters/regional_ventilation_delay.py
+++ eitprocessing/parameters/regional_ventilation_delay.py
@@ -7,6 +7,8 @@ from . import ParameterExtraction
 @dataclass
 class RegionalVentilationDelay(ParameterExtraction):
     breath_detection_kwargs: dict = {}
+    interpolation_kind = "cubic"  # Type of interpolation used for normalizing all inspirations between 0 and 1
+    common_time = np.linspace(0, 1, 101)  # Common time axis to which is normalized
 
     def compute_parameter(self, sequence, frameset_name: str) -> tuple[NDArray, list]:
         """Computes the regional ventilation delay per breath using pixel
@@ -20,35 +22,39 @@ class RegionalVentilationDelay(ParameterExtraction):
 
         pixel_impedance = sequence.framesets[frameset_name].pixel_values
 
-    # interpolated_signals = []
-    # for start, end in zip(start_insp, end_insp):
+        interpolated_inspirations = []
+        for breath in breaths:
+            num_timepoints = breath.middle_index - breath.start_index
 
-    #     num_timepoints = end - start
+            # By dividing by (num_timepoints - 1) the time axis starts at 0 and ends at 1
+            norm_time = np.arange(num_timepoints) / (num_timepoints - 1)
 
-    #     # By dividing by (num_timepoints - 1) the time axis starts at 0 and ends at 1
-    #     norm_time = np.arange(num_timepoints) / (num_timepoints - 1)
+            pixel_inspiration = pixel_impedance[
+                breath.start_index : breath.middle_index, :, :
+            ]
 
-    #     inspiration = all_pixels[start:end, :, :]
+            def interpolate(y):
+                return interp1d(norm_time, y, kind=self.interpolation_kind)(
+                    self.common_time
+                )
 
-    #     def interpolate(y):
-    #         return interp1d(norm_time, y, kind=INTERPOLATION_KIND)(COMMON_TIME)
+            # This applies interpolation for every pixel
+            interpolated_inspiration = np.apply_along_axis(
+                interpolate, 0, pixel_inspiration
+            )
+            interpolated_inspirations.append(interpolated_inspiration)
 
-    #     # This applies interpolation for every pixel
-    #     interpolated_imp = np.apply_along_axis(interpolate, 0, inspiration)
-    #     interpolated_signals.append(interpolated_imp)
+        # Mean inspiration for each pixel
+        mean_pixel_inspiration = np.mean(interpolated_inspirations, axis=0)
 
-    # # Mean inspiration for each pixel
-    # pix_matrix = np.mean(interpolated_signals, axis=0)
-
-    ## Code below should be adjusted to ALIVE format
-    # offset = pix_matrix[0,:,:]
-    # pix_matrix -= offset
-    # total_imp = pix_matrix.max(axis=0)
-    # norm_pix_imp = (pix_matrix - 0)/ (total_imp-0)
-    # rvd = np.argmax(norm_pix_imp > 0.4, axis=0)
-    # non_zero_rvd = rvd[rvd != 0]
-    # vmin = np.min(non_zero_rvd)
-    # vmax = np.max(rvd)
+        offset = mean_pixel_inspiration[0, :, :]
+        mean_pixel_inspiration -= offset
+        max_impedance = mean_pixel_inspiration.max(axis=0)
+        normalized_pixel_tiv = (mean_pixel_inspiration - 0) / (max_impedance - 0)
+        rvd = np.argmax(normalized_pixel_tiv > 0.4, axis=0)
+        non_zero_rvd = rvd[rvd != 0]
+        vmin = np.min(non_zero_rvd)
+        vmax = np.max(rvd)
 
     # plt.title(f"Regional ventilation delay map at {timepoint}")
     # plt.imshow(rvd, vmin=vmin, vmax=vmax)
@@ -59,8 +65,8 @@ class RegionalVentilationDelay(ParameterExtraction):
 
     # for row in range(0,32):
     #     for col in range (0,32):
-    #         if isinstance(pix_matrix[row,col], np.ndarray):
-    #             plt.plot(COMMON_TIME, norm_pix_imp[:,row,col])
+    #         if isinstance(mean_pixel_inspiration[row,col], np.ndarray):
+    #             plt.plot(COMMON_TIME, normalized_pixel_tiv[:,row,col])
     #             plt.title(f'Normalized pixel impedance of all ventilated pixels at {timepoint}')
     #             plt.xlabel('Normalized time')
     #             plt.ylabel('Normalized impedance')
