3c3a59fafe2a62fa0ea856f77cbe195e80cd5579
Peter Somhorst <p.somhorst@erasmusmc.nl>
2023-11-14 16:58:42 +0100

    Update Reader class for better typing

--- eitprocessing/binreader/reader.py
+++ eitprocessing/binreader/reader.py
@@ -5,39 +5,70 @@ Licensed under the Apache License, version 2.0. See LICENSE for details.
 This file contains methods related to when electrical impedance tomographs are read.
 """
 
+import io
 import struct
+from collections.abc import Callable
+from dataclasses import dataclass
+from functools import partial
 from functools import partialmethod
+from typing import Any
+from typing import TypeVar
 import numpy as np
+from numpy.typing import NDArray
 
 
+T = TypeVar("T")
+N = TypeVar("N", bound=np.number)
+
+
+@dataclass
 class Reader:
-    file_handle = None
+    file_handle: io.BufferedReader
 
-    def __init__(self, file_handle):
-        self.file_handle = file_handle
+    def read_single(self, type_code: str, cast: type[T]) -> T:
+        data = self._read_full_type_code(type_code)
+        return cast(data[0])
 
-    def read(self, type_code, length=1, cast=None):
-        if len(type_code) > 1:
-            full_type_code = type_code * length
-        else:
-            full_type_code = f"{length}{type_code}"
+    def read_list(self, type_code: str, cast: type[T], length: int) -> list[T]:
+        full_type_code = f"{length}{type_code}"
+        data = self._read_full_type_code(full_type_code)
+        data = [cast(d) for d in data]
+        return data
+
+    def read_array(self, type_code: str, cast: type[N], length: int) -> NDArray[N]:
+        full_type_code = f"{length}{type_code}"
+        data = self._read_full_type_code(full_type_code)
+        return np.array(data, dtype=cast)
+
+    def read_string(self, length=1):
+        full_type_code = f"{length}s"
+        data = self._read_full_type_code(full_type_code)
+        data = data[0].decode().rstrip()
+        return data
+
+    def _read_full_type_code(self, full_type_code) -> tuple[Any, ...]:
         data_size = struct.calcsize(full_type_code)
         packed_data = self.file_handle.read(data_size)
         data = struct.unpack(full_type_code, packed_data)
-
-        if length == 1:
-            data = data[0]
-
-        if cast:
-            return cast(data)
-
         return data
 
-    @staticmethod
-    def cast_string(data):
-        return data[0].decode().rstrip()
+    def float32(self) -> float:
+        return self.read_single(type_code="f", cast=float)
 
-    float32 = partialmethod(read, type_code="f", cast=np.float32)
-    float64 = partialmethod(read, type_code="d", cast=np.float64)
-    int32 = partialmethod(read, type_code="i", cast=np.int32)
-    string = partialmethod(read, type_code="s", cast=cast_string)
+    def float64(self) -> float:
+        return self.read_single(type_code="d", cast=float)
+
+    def npfloat32(self, length=1) -> NDArray[np.float32]:
+        return self.read_array(type_code="f", cast=np.float32, length=length)
+
+    def npfloat64(self, length=1) -> NDArray[np.float64]:
+        return self.read_array(type_code="d", cast=np.float64, length=length)
+
+    def int32(self) -> int:
+        return self.read_single(type_code="i", cast=int)
+
+    def npint32(self, length=1) -> NDArray[np.int32]:
+        return self.read_array(type_code="i", cast=np.int32, length=length)
+
+    def string(self, length=1) -> str:
+        return self.read_string(length=length)
