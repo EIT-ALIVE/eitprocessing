cfe7017385bfbc01115c6cdc9094d62f97298aa0
Peter Somhorst <p.somhorst@erasmusmc.nl>
2023-11-24 13:36:12 +0100

    Add tests for Variant

---
+++ tests/test_variant.py
@@ -0,0 +1,127 @@
+from dataclasses import dataclass
+from dataclasses import field
+from unittest.mock import patch
+import pytest
+from eitprocessing.helper import NotEquivalent
+from eitprocessing.variants import Variant
+
+
+@pytest.fixture
+def variant_a(VariantSubA, make_params):
+    return VariantSubA("label_a", "description_a", params=make_params(), data=[1, 2, 3])
+
+
+@pytest.fixture
+def variant_a_copy(VariantSubA, make_params):
+    return VariantSubA("label_a", "description_a", params=make_params(), data=[1, 2, 3])
+
+
+@pytest.fixture
+def variant_b(VariantSubA, make_params):
+    return VariantSubA("label_b", "description_b", make_params(), data=[4, 5, 6])
+
+
+@pytest.fixture
+def variant_c(VariantSubB, make_params):
+    return VariantSubB("label_b", "description_b", make_params(), data=[4, 5, 6])
+
+
+@pytest.fixture
+def make_params():
+    def _make_params():
+        return dict(
+            some_string="string", some_dict=dict(some_int=5, some_string="string 2")
+        )
+
+    return _make_params
+
+
+@pytest.fixture
+def VariantSubA():
+    @dataclass
+    class VariantSubA(Variant):
+        data: list = field(repr=False, kw_only=True)
+
+        @classmethod
+        def concatenate(cls, a, b):
+            return super().concatenate(a, b)
+
+    return VariantSubA
+
+
+@pytest.fixture
+def VariantSubB():
+    @dataclass
+    class VariantSubB(Variant):
+        data: list = field(repr=False, kw_only=True)
+
+        @classmethod
+        def concatenate(cls, a, b):
+            return super().concatenate(a, b)
+
+    return VariantSubB
+
+
+def test_init(VariantSubA, make_params):
+    _ = VariantSubA("label", "description", data=[])
+    _ = VariantSubA("label", "description", make_params(), data=[])
+    _ = VariantSubA("label", "description", data=[], params=make_params())
+
+    with pytest.raises(TypeError):
+        # you should not be able to initialize the abstract base class Variant
+        _ = Variant("label", "description")
+
+    with pytest.raises(TypeError):
+        # you should not be able to initialize without label, description and data
+        _ = VariantSubA()
+
+    with pytest.raises(TypeError):
+        # you should not be able to initialize without description and data
+        _ = VariantSubA("label")
+
+    with pytest.raises(TypeError):
+        # you should not be able to initialize without description and data
+        _ = VariantSubA(label="label")
+
+    with pytest.raises(TypeError):
+        # you should not be able to initialize without label and data
+        _ = VariantSubA(description="description")
+
+    with pytest.raises(TypeError):
+        _ = VariantSubA("label", "description")
+
+
+def test_equivalence(variant_a, variant_a_copy, variant_b, variant_c):
+    assert variant_a is not variant_a_copy  # objects are not the same
+    assert variant_a == variant_a_copy  # objects contain same attribute values
+    assert variant_a != variant_b  # objects have different attribute values
+
+    variant_a_copy.data = [4, 5, 6]
+    assert variant_a != variant_a_copy  # objects contain different data
+    # objects are still equivalent
+    assert Variant.check_equivalence(variant_a, variant_a_copy)
+
+    # objects with different attributes are not equivalent
+    variant_a_copy.label = "different label"
+    assert not Variant.check_equivalence(variant_a, variant_a_copy)
+    variant_a_copy.label = variant_a.label
+
+    # objects with different attributes are not equivalent
+    variant_a_copy.description = "different description"
+    assert not Variant.check_equivalence(variant_a, variant_a_copy)
+    variant_a_copy.description = variant_a.description
+
+    # objects with different parameters are not equivalent
+    variant_a_copy.params["some_dict"]["some_string"] = "another string"
+    assert not Variant.check_equivalence(variant_a, variant_a_copy)
+    variant_a_copy.params["some_dict"]["some_string"] = variant_a.params["some_dict"][
+        "some_string"
+    ]
+
+    # objects with different attribute values are not equivalent
+    assert not Variant.check_equivalence(variant_a, variant_b)
+    with pytest.raises(NotEquivalent):
+        assert not Variant.check_equivalence(variant_a, variant_b, raise_=True)
+
+    # objects with different classes are not equivalent
+    assert not Variant.check_equivalence(variant_b, variant_c)
