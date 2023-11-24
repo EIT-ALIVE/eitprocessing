c984d5820bf6e99941698dde25c438ceb63b4807
Peter Somhorst <p.somhorst@erasmusmc.nl>
2023-11-24 12:33:24 +0100

    Add documentation for Variant and VariantCollection

--- eitprocessing/variants/__init__.py
+++ eitprocessing/variants/__init__.py
@@ -13,19 +13,51 @@ T = TypeVar("T", bound="Variant")
 
 @dataclass
 class Variant(ABC):
-    name: str
+    """Contains a single variant of a dataset.
+
+    A variant of a dataset is defined as either the raw data, or an edited
+    version of that raw data. For example, EIT data can contain a "raw"
+    versions and a "filtered" version. Both variants share the same time axis.
+
+    The actual data of a variant is contained in a variable that must be set by
+    a subclass inheriting from this class.
+
+    Attributes:
+    - label (str): a short descriptor for the variant, that is used to access
+      the variant
+    - description (str): a longer description of the variant
+    - params (dict): contains information on how to reproduce the variant, e.g.
+      which filters and filters settigs were used
+    """
+
+    label: str
     description: str
     params: dict = field(default_factory=dict)
 
     @staticmethod
     def check_equivalence(a: T, b: T, raise_=False) -> bool:
-        try:
+        """Check the equivalence of two variants
+
+        For two variants to be equivalent, they need to have the same class,
+        the same label, the same description and the same parameters. Only the
+        actual data can differ between variants.
+
+        Args:
+        - a (Variant)
+        - b (Variant)
+
+        Raises:
+        - NotEquivalent (only if raise_ is `True`) when a and b are not
+          equivalent on one of the attributes
+        """
+        cm = contextlib.nullcontext() if raise_ else contextlib.suppress(NotEquivalent)
+        with cm:
             if not isinstance(a, b.__class__):
                 raise NotEquivalent(
                     f"Variant classes don't match: {a.__class__}, {b.__class__}"
                 )
 
-            if (a_ := a.name) != (b_ := b.name):
+            if (a_ := a.label) != (b_ := b.label):
                 raise NotEquivalent(f"EITDataVariant names don't match: {a_}, {b_}")
 
             if (a_ := a.description) != (b_ := b.description):
@@ -43,4 +75,18 @@ class Variant(ABC):
     @classmethod
     @abstractmethod
     def concatenate(cls, a: Self, b: Self) -> Self:
-        ...
+        """Concatenates two variants
+
+        Concatenating two variants results in a single variant with the
+        combined length of both variants.
+
+        To merge more than two variants, use
+        `functools.reduce(Variant.concatenate, list_of_variants)`.
+
+        Args:
+        - a (Variant)
+        - b (Variant)
+
+        Raises:
+        - NotEquivalent if a and b are not equivalent and can't be merged
+        """
--- eitprocessing/variants/variant_collection.py
+++ eitprocessing/variants/variant_collection.py
@@ -10,6 +10,45 @@ V = TypeVar("V", bound="Variant")
 
 
 class VariantCollection(dict, Generic[V]):
+    """A collection of variants of a single type
+
+    A VariantCollection is a dictionary with some added features.
+
+    A VariantCollection can only contain variants (of a certain type). When
+    initializing VariantCollection a subclass of Variant (or Variant itself to
+    allow diverse types )must be passed as the first argument (`variant_type`),
+    limiting the type of variant that is allowed to be added to the collection.
+    During initialization, other arguments can be passed as if initializing a
+    normal dictionary.
+
+    When adding a variant, the key of the item in the dictionary must equal the
+    label of the variant. The method `add()` can be used instead, which
+    automatically sets the variant label as the key.
+
+    Thirdly, when setting a variant with a label that already exists, the
+    default behaviour is to raise an exception. This prevents overwriting
+    existing variants. This behaviour can be overridden using `add(variant,
+    overwrite=True)`.
+
+
+    The `add()` method
+
+    Examples:
+    ```
+    >>> variant_a = EITDataVariant(label="raw", ...)
+    >>> vc = VariantCollection(EITDataVariant, raw=variant_a)
+    >>> variant_b = EITDataVariant(label="filtered", ...)
+    >>> vc.add(variant_b)  # equals vc["filtered"] = variant
+    ```
+
+    ```
+    >>> vc = VariantCollection(EITDataVariant)
+    >>> variant_c = SomeOtherVariant(label="offset", ...)
+    >>> vc.add(variant_c)  # raises InvalidVariantType() exception
+    ```
+
+    """
+
     variant_type: type[V]
 
     def __init__(self, variant_type: type[V], *args, **kwargs):
@@ -20,21 +59,37 @@ class VariantCollection(dict, Generic[V]):
         self._check_variant(__value, key=__key)
         return super().__setitem__(__key, __value)
 
-    def add(self, variant: V, overwrite: bool = False) -> None:
-        self._check_variant(variant, overwrite=overwrite)
-        return super().__setitem__(variant.name, variant)
+    def add(self, *variant: V, overwrite: bool = False) -> None:
+        """Add one or multiple variants to the dictionary
+
+        This method automatically sets the key of the item to the label of the
+        variant. By default, overwriting variants with the same label is
+        prevented. Trying to do so will result in a DuplicateVariantLabel
+        exception being raised. Set `overwrite` to `True` to allow overwriting.
+
+        Args:
+        - variant (Variant): the variant to be added. Multiple variants can be
+          added at once.
+
+        Raises:
+        - DuplicateVariantLabel if one attempts to add a variant with a label
+          that already exists as key.
+        """
+        for variant_ in variant:
+            self._check_variant(variant_, overwrite=overwrite)
+            return super().__setitem__(variant_.label, variant_)
 
     def _check_variant(self, variant: V, key=None, overwrite: bool = False) -> None:
-        if self.variant_type and not isinstance(variant, self.variant_type):
+        if not isinstance(variant, self.variant_type):
             raise InvalidVariantType(
                 f"'{type(variant)}' does not match '{self.variant_type}'."
             )
 
-        if key and key != variant.name:
-            raise KeyError(f"'{key}' does not match variant name '{variant.name}'.")
+        if key and key != variant.label:
+            raise KeyError(f"'{key}' does not match variant name '{variant.label}'.")
 
         if not overwrite and key in self:
-            raise DuplicateVariantName(
+            raise DuplicateVariantLabel(
                 f"Variant with name {key} already exists. Use `overwrite=True` to overwrite."
             )
 
@@ -73,9 +128,9 @@ class VariantCollection(dict, Generic[V]):
         return False
 
 
-class InvalidVariantType(Exception):
+class InvalidVariantType(TypeError):
     """Raised when a variant that does not match the variant type is added."""
 
 
-class DuplicateVariantName(Exception):
+class DuplicateVariantLabel(KeyError):
     """Raised when a variant with the same name already exists in the collection."""
