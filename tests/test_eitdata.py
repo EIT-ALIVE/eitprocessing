cd65d82ab445e9a47f0e09272e4d4e38db01e480
Walter Baccinelli <w.baccinelli@esciencecenter.nl>
2023-12-13 14:23:28 +0100

    test ensure vendor

--- tests/test_eitdata.py
+++ tests/test_eitdata.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from eitprocessing.eit_data import EITData
+from eitprocessing.eit_data import NoVendorProvided, UnknownVendor
 from eitprocessing.eit_data.draeger import DraegerEITData
 from eitprocessing.eit_data.timpel import TimpelEITData
 from eitprocessing.eit_data.sentec import SentecEITData
@@ -75,3 +76,53 @@ def test_get_vendor_class_timpel():
 def test_get_vendor_class_invalid_vendor():
     with pytest.raises(KeyError):
         EITData._get_vendor_class("unexistent_key")
+
+
+def test_ensure_vendor_valid_draeger():
+    vendor_enum = Vendor.DRAEGER
+    vendor_string = "draeger"
+
+    result_enum = EITData._ensure_vendor(vendor_enum)
+    result_string = EITData._ensure_vendor(vendor_string)
+
+    assert result_enum == vendor_enum
+    assert result_string == vendor_enum
+
+
+def test_ensure_vendor_valid_sentec():
+    vendor_enum = Vendor.SENTEC
+    vendor_string = "sentec"
+
+    result_enum = EITData._ensure_vendor(vendor_enum)
+    result_string = EITData._ensure_vendor(vendor_string)
+
+    assert result_enum == vendor_enum
+    assert result_string == vendor_enum
+
+
+def test_ensure_vendor_valid_timpel():
+    vendor_enum = Vendor.TIMPEL
+    vendor_string = "timpel"
+
+    result_enum = EITData._ensure_vendor(vendor_enum)
+    result_string = EITData._ensure_vendor(vendor_string)
+
+    assert result_enum == vendor_enum
+    assert result_string == vendor_enum
+
+
+def test_ensure_vendor_invalid_vendor_string():
+    with pytest.raises(UnknownVendor):
+        EITData._ensure_vendor("unknown_vendor")
+
+    with pytest.raises(NoVendorProvided):
+        EITData._ensure_vendor("")
+
+    with pytest.raises(NoVendorProvided):
+        EITData._ensure_vendor(None)
+
+
+# TODO: add also other types?
+# def test_ensure_vendor_invalid_vendor_type():
+#     with pytest.raises(TypeError, match="must be a str or Vendor enum"):
+#         EITData._ensure_vendor(123)
