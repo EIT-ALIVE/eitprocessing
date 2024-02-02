834b09948892f9407a86aa1962438ec643df81b6
Peter Somhorst <p.somhorst@erasmusmc.nl>
2024-02-02 17:06:13 +0100

    Rename `calculated_from` to `derived_from`

--- eitprocessing/continuous_data/__init__.py
+++ eitprocessing/continuous_data/__init__.py
@@ -10,12 +10,12 @@ class ContinuousData:
     label: str
     name: str
     unit: str
-    calculated_from: Any | list[Any] | None = None
     description: str = ""
     parameters: dict[str, Any] = field(default_factory=dict)
     loaded: bool = False
+    derived_from: Any | list[Any] = field(default_factory=list)
     values: np.ndarray = field(kw_only=True)
 
     def __post_init__(self):
-        if not self.loaded and not self.calculated_from:
+        if not self.loaded and not self.derived_from:
             raise ValueError("Data must be loaded or calculated form another dataset.")
