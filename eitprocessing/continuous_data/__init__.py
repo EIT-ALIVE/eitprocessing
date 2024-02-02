7bac1a73b26edf08648ed723866610e20d106abf
Peter Somhorst <p.somhorst@erasmusmc.nl>
2024-02-02 17:05:37 +0100

    Remove merge ContinuousData and ContinuousDataVariant, removing the latter

--- eitprocessing/continuous_data/__init__.py
+++ eitprocessing/continuous_data/__init__.py
@@ -1,24 +1,20 @@
 from dataclasses import dataclass
 from dataclasses import field
-from typing import Any
 import numpy as np
-from numpy.typing import NDArray
+from typing_extensions import Any
 from typing_extensions import Self
-from eitprocessing.continuous_data.continuous_data_variant import ContinuousDataVariant
-from eitprocessing.mixins.equality import Equivalence
-from eitprocessing.variants.variant_collection import VariantCollection
 
 
-@dataclass(eq=False)
-class ContinuousData(Equivalence):
+@dataclass
+class ContinuousData:
+    label: str
     name: str
     unit: str
-    description: str
-    loaded: bool
     calculated_from: Any | list[Any] | None = None
-    variants: VariantCollection = field(
-        default_factory=lambda: VariantCollection(ContinuousDataVariant)
-    )
+    description: str = ""
+    parameters: dict[str, Any] = field(default_factory=dict)
+    loaded: bool = False
+    values: np.ndarray = field(kw_only=True)
 
     def __post_init__(self):
         if not self.loaded and not self.calculated_from:
