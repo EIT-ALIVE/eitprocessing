7c632d22edadfa037efff5787429101f8aabb90e
Dani Bodor <d.bodor@esciencecenter.nl>
2024-02-05 03:24:56 +0100

    docs: improve documentation for slicing mixin

--- eitprocessing/eit_data/eit_data_variant.py
+++ eitprocessing/eit_data/eit_data_variant.py
@@ -1,10 +1,3 @@
-"""
-Copyright 2023 Netherlands eScience Center and Erasmus University Medical Center.
-Licensed under the Apache License, version 2.0. See LICENSE for details.
-
-This file contains methods related to when electrical impedance tomographs are read.
-"""
-
 from dataclasses import dataclass
 from dataclasses import field
 import numpy as np
@@ -70,7 +63,10 @@ class EITDataVariant(Variant, SelectByTime):
         )
 
     def _sliced_copy(
-        self, start_index: int, end_index: int, label: str | None = None
+        self,
+        start_index: int,
+        end_index: int,
+        label: str | None = None,
     ) -> Self:
         pixel_impedance = self.pixel_impedance[start_index:end_index, :, :]
 
