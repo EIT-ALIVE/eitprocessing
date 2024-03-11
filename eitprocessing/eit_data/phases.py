6beba5891476b59c6c4b862c14fa5cffdcec8059
Dani Bodor <d.bodor@esciencecenter.nl>
2024-03-11 13:27:19 +0100

    add missing docstrings

--- eitprocessing/eit_data/phases.py
+++ eitprocessing/eit_data/phases.py
@@ -3,20 +3,22 @@ from dataclasses import dataclass
 
 @dataclass
 class PhaseIndicator:
+    """Parent class for phase indications."""
+
     index: int
     time: float
 
 
 @dataclass
 class MinValue(PhaseIndicator):
-    pass
+    """Automatically registered local minimum of an EIT measurement."""
 
 
 @dataclass
 class MaxValue(PhaseIndicator):
-    pass
+    """Automatically registered local maximum of an EIT measurement."""
 
 
 @dataclass
 class QRSMark(PhaseIndicator):
-    pass
+    """Automatically registered QRS mark an EIT measurement from a Timpel device."""
