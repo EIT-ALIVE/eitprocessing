2b7c1240662cfca33534dfc41af282d6151b4da4
Peter Somhorst <p.somhorst@erasmusmc.nl>
2024-03-15 12:50:58 +0100

    Refactor load_from to load_data, now only returning all three types of data

--- eitprocessing/eit_data/loading/__init__.py
+++ eitprocessing/eit_data/loading/__init__.py
@@ -1,20 +1,29 @@
 from functools import reduce
 from pathlib import Path
+from typing import NamedTuple
 
+from eitprocessing.continuous_data import ContinuousData
 from eitprocessing.data_collection import DataCollection
 from eitprocessing.eit_data import EITData
-from eitprocessing.eit_data.loading import draeger, sentec, timpel
 from eitprocessing.eit_data.vendor import Vendor
+from eitprocessing.sparse_data import SparseData
 
 
-def load_from(
+class LoadDataResult(NamedTuple):
+    """Result from load_data(), containing three DataCollections with EITData, ContinuousData and SparseData."""
+
+    eit_data: DataCollection[str, EITData]
+    continuous_data: DataCollection[str, ContinuousData]
+    sparse_data: DataCollection[str, SparseData]
+
+
+def load_data(
     path: str | Path | list[str | Path],
     vendor: Vendor | str,
     framerate: float | None = None,
     first_frame: int = 0,
     max_frames: int | None = None,
-    return_non_eit_data: bool = False,
-) -> DataCollection | tuple[DataCollection, DataCollection, DataCollection]:
+) -> tuple[DataCollection[EITData], DataCollection[ContinuousData], DataCollection[SparseData]]:
     """Load EIT data from path(s).
 
     Args:
@@ -31,19 +40,22 @@ def load_from(
         max_frames: maximum number of frames to load.
             The actual number of frames can be lower than this if this
             would surpass the final frame.
-        return_non_eit_data: whether to load available continuous and sparse data.
 
     Raises:
         NotImplementedError: is raised when there is no loading method for
         the given vendor.
 
     Returns:
-        EITData: container for the loaded data and metadata from all files in path.
+        LoadDataResult: a named tuple containing a DataCollection of EITData (result.eit_data or result[0]), a
+        DataCollection of ContinuousData (result.continuous_data or result[1]) and a DataCollection of SparseData
+        (result.sparse_data or result[2]).
     """
+    from eitprocessing.continuous_data import ContinuousData
     from eitprocessing.data_collection import DataCollection
+    from eitprocessing.eit_data.loading import draeger, sentec, timpel
+    from eitprocessing.sparse_data import SparseData
 
     vendor = _ensure_vendor(vendor)
-
     load_func = {
         "draeger": draeger.load_from_single_path,
         "timpel": timpel.load_from_single_path,
@@ -67,39 +79,27 @@ def load_from(
             framerate=framerate,
             first_frame=first_frame,
             max_frames=max_frames,
-            return_non_eit_data=return_non_eit_data,
         )
 
-        if return_non_eit_data:
-            from eitprocessing.continuous_data import ContinuousData
-            from eitprocessing.sparse_data import SparseData
-
-            if type(loaded_data) is not tuple:
-                eit = loaded_data
-                continuous = DataCollection(ContinuousData)
-                sparse = DataCollection(SparseData)
-            else:
-                eit, continuous, sparse = loaded_data
-
-            eit: DataCollection
-            continuous: DataCollection
-            sparse: DataCollection
-
-            eit_datasets.append(eit)
-            continuous_datasets.append(continuous)
-            sparse_datasets.append(sparse)
-
+        if type(loaded_data) is not tuple:
+            eit = loaded_data
+            continuous = DataCollection(ContinuousData)
+            sparse = DataCollection(SparseData)
         else:
-            assert isinstance(loaded_data, DataCollection)  # noqa: S101
-            eit_datasets.append(loaded_data)
+            eit, continuous, sparse = loaded_data
 
-    if return_non_eit_data:
-        return tuple(
-            reduce(DataCollection.concatenate, datasets)
-            for datasets in (eit_datasets, continuous_datasets, sparse_datasets)
-        )
+        eit: DataCollection
+        continuous: DataCollection
+        sparse: DataCollection
 
-    return reduce(DataCollection.concatenate, eit_datasets)
+        eit_datasets.append(eit)
+        continuous_datasets.append(continuous)
+        sparse_datasets.append(sparse)
+
+    return LoadDataResult(
+        reduce(DataCollection.concatenate, datasets)
+        for datasets in (eit_datasets, continuous_datasets, sparse_datasets)
+    )
 
 
 def _check_first_frame(first_frame: int | None) -> int:
