3d0bfaa754f2ae36f3f2c793657feba2ecd06df5
Peter Somhorst <p.somhorst@erasmusmc.nl>
2024-03-15 14:37:30 +0100

    Linting and formatting

--- eitprocessing/parameters/eeli.py
+++ eitprocessing/parameters/eeli.py
@@ -1,39 +1,44 @@
+from collections.abc import Callable
+
 import numpy as np
-from numpy.typing import NDArray
-from ..features import BreathDetection
-from . import ParameterExtraction
+
+from eitprocessing.features import BreathDetection
+from eitprocessing.parameters import ParameterExtraction
 
 
 class EELI(ParameterExtraction):
     def __init__(
         self,
-        regions: list[NDArray] | None = None,
+        regions: list[np.ndarray] | None = None,
         detect_breaths_method: str = "Extreme values",
-        summary_stats: dict[str, Callable[[NDArray], float]] = {
-            "per breath": lambda x: x,
-            "mean": np.mean,
-            "standard deviation": np.std,
-            "median": np.median,
-        },
+        summary_stats: dict[str, Callable[[np.ndarray], float]] | None = None,
     ):
+        if summary_stats is None:
+            summary_stats = {
+                "per breath": lambda x: x,
+                "mean": np.mean,
+                "standard deviation": np.std,
+                "median": np.median,
+            }
         self.detect_breaths_method = detect_breaths_method
         self.regions = regions
         self.summary_stats = summary_stats
 
     def compute_parameter(self, sequence, frameset_name: str) -> dict | list[dict]:
         """Computes the end-expiratory lung impedance (EELI) per breath in the
-        global impedance."""
-
+        global impedance.
+        """
         breath_detector = BreathDetection(
-            sequence.framerate, **self.breath_detection_kwargs
+            sequence.framerate,
+            **self.breath_detection_kwargs,
         )
         breaths = breath_detector.find_breaths(global_impedance)
 
-        _, _, end_indices = (np.array(indices) for indices in zip(breaths))
+        _, _, end_indices = (np.array(indices) for indices in zip(breaths, strict=False))
 
         if self.regions is None:
             global_impedance = sequence.framesets[frameset_name].global_impedance
-            global_eelis: NDArray = global_impedance[end_indices]
+            global_eelis: np.ndarray = global_impedance[end_indices]
 
             global_eeli = {}
             for name, function in self.summary_stats.items():
@@ -46,7 +51,7 @@ class EELI(ParameterExtraction):
             for region in self.regions:
                 regional_pixel_impedance = np.matmul(pixel_impedance, region)
                 regional_impedance = np.nansum(regional_pixel_impedance, axis=(0, 1))
-                regional_eelis: NDArray = regional_impedance[end_indices]
+                regional_eelis: np.ndarray = regional_impedance[end_indices]
                 results = {}
                 for name, function in self.summary_stats.items():
                     results[name] = function(regional_eelis)
