cb2d33d9554d30e8c6dbcfffce098589e0dbc5d9
Peter Somhorst <p.somhorst@erasmusmc.nl>
2024-03-19 08:58:36 +0100

    Create SparseData object

--- eitprocessing/datahandling/sparsedata.py
+++ eitprocessing/datahandling/sparsedata.py
@@ -4,23 +4,22 @@ from typing import Any
 import numpy as np
 from typing_extensions import Self
 
-from eitprocessing.datahandling.mixins.slicing import SelectByTime
+from eitprocessing.mixins.slicing import SelectByTime
 
 
 @dataclass
 class SparseData(SelectByTime):
-    """Container for data occuring at unpredictable time points.
+    """Container for single value or sparse data.
 
-    In sparse data the time points are not necessarily evenly spaced. Data can consist time-value pairs or only time
-    points. Values generally are numeric values in arrays, but can also be lists of different types of object.
+    Sparse data is data that does not appear/change at predictable regular time intervals. Examples are detected breaths
+    or heart beats, tidal impedance variations per breath, or a single PEEP value for a PEEP step during a decremental
+    PEEP trial.
 
-    Sparse data differs from IntervalData in that each data points is associated with a single time point rather than a
-    time range.
+    Sparse data can consist of only time (e.g. detected QRS complexes), time-value pairs (e.g. tidal impedance variation
+    of breaths at the end of each breath), values without time (e.g. objects) or a single value (e.g. the PEEP level of
+    the current PEEP step).
 
-    Examples are data points at end of inspiration/end of expiration (e.g. tidal volume, end-expiratoy lung impedance)
-    or detected time points (e.g. QRS complexes).
-
-
+    Either the value or values can be provided, but not both.
 
     Args:
         label: Computer readable name.
@@ -31,20 +30,28 @@ class SparseData(SelectByTime):
         parameters: Parameters used to derive the data.
         derived_from: Traceback of intermediates from which the current data was derived.
         values: List or array of values. These van be numeric data, text or Python objects.
+        value: single value. This van be numeric, text or a Python object.
     """
 
     label: str
     name: str
     unit: str | None
     category: str
-    time: np.ndarray | None
     description: str = ""
     parameters: dict[str, Any] = field(default_factory=dict)
     derived_from: list[Any] = field(default_factory=list)
+    time: np.ndarray | None = None
     values: Any | None = None
+    value: Any | None = None
 
     def __post_init__(self) -> None:
-        pass
+        if self.values and self.value:
+            msg = "Can't store `value` and `values`. Only provided one of them."
+            raise ValueError(msg)
+
+        if all((self.time is None, self.value is None, self.values is None)):
+            msg = "No data provided. Provided (time and) values or value."
+            raise ValueError(msg)
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}('{self.label}')"
@@ -57,7 +64,7 @@ class SparseData(SelectByTime):
     ) -> Self:
         # TODO: check correct implementation
         cls = self.__class__
-        time = self.time[start_index:end_index]
+        time = self.time[start_index:end_index] if self.time is not None else None
         values = self.values[start_index:end_index] if self.values is not None else None
         description = f"Slice ({start_index}-{end_index}) of <{self.description}>"
 
@@ -70,4 +77,5 @@ class SparseData(SelectByTime):
             derived_from=[*self.derived_from, self],
             time=time,
             values=values,
+            value=self.value,
         )
