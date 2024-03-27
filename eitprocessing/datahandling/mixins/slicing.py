22cb84d9fadfde682efddecb6aba148573d12379
Peter Somhorst <p.somhorst@erasmusmc.nl>
2024-03-27 19:05:35 +0100

    Set `start_inclusive` to False by default

--- eitprocessing/datahandling/mixins/slicing.py
+++ eitprocessing/datahandling/mixins/slicing.py
@@ -104,7 +104,7 @@ class SelectByTime(SelectByIndex, HasTimeIndexer):
         self,
         start_time: float | None = None,
         end_time: float | None = None,
-        start_inclusive: bool = True,
+        start_inclusive: bool = False,
         end_inclusive: bool = False,
         label: str | None = None,
     ) -> Self:
