8a9da64cf6c04d4343e4105ea0552dc17141db07
Peter Somhorst <p.somhorst@erasmusmc.nl>
2024-03-28 13:33:26 +0100

    Refactor Breath to contain times instead of indices

--- eitprocessing/datahandling/breath.py
+++ eitprocessing/datahandling/breath.py
@@ -4,6 +4,6 @@ from typing import NamedTuple
 class Breath(NamedTuple):
     """Represents a breath with a start, middle and end index."""
 
-    start_index: int
-    middle_index: int
-    end_index: int
+    start_time: float
+    middle_time: float
+    end_time: float
