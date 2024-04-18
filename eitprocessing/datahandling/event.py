fe705d2de5c71353bf758c980485df7fb84a1fd6
Peter Somhorst <p.somhorst@erasmusmc.nl>
2024-04-18 16:34:10 +0200

    Remove index and time from Event

--- eitprocessing/datahandling/event.py
+++ eitprocessing/datahandling/event.py
@@ -5,7 +5,5 @@ from dataclasses import dataclass
 class Event:
     """Single time point event registered during an EIT measurement."""
 
-    index: int
-    time: float
     marker: int
     text: str
