e942e99e53e161c45eaa3f48f168221877589ef2
Peter Somhorst <p.somhorst@erasmusmc.nl>
2024-06-20 17:19:27 +0200

    Expand documentation of MovingAverage

--- eitprocessing/features/moving_average.py
+++ eitprocessing/features/moving_average.py
@@ -6,26 +6,23 @@ import numpy as np
 
 @dataclass
 class MovingAverage:
-    """Calculate the moving average of the data.
+    """Algorithm for calculating the moving average of the data.
 
-    The moving average is calculated using a convolution with a window. The
-    window length (in seconds) is determined by the attribute
-    `averaging_window_duration`. The shape of the window is determined by
-    `averaging_window_fun`, which should be a callable that takes an
-    integer `M` and returns an array-like sequence containing a window with
-    length `M` and area 1.
+    This class provides a method for calculating of the moving average of a 1D signal by convolution with a window with
+    a given size. If not window function is provided, all samples within that window contribute equally to the moving
+    average. If a window function is provided, the samples are weighed according to the values in the window function.
 
-    Before convolution the data is padded. The padding type is 'edge' by
-    default. See `np.pad()` for more information. Padding adds values at the
-    start and end with the first/last value, to more accurately determine the
-    average at the boundaries of the data.
+    Before convolution the data is padded. The padding type is 'edge' by default. See `np.pad()` for more information.
+    Padding adds values at the start and end with the first/last value, to more accurately determine the average at the
+    boundaries of the data.
 
     Args:
-      window_size: the size of the window. Is enlarged by 1 if uneven.
-      window_fun: window function, e.g. np.window.bartlett.
+        window_size: the size of the window. Should be odd; is enlarged by 1 if even.
+        window_fun: window function, e.g. np.window.bartlett.
+        padding_type: see `np.pad()`.
 
     Returns:
-        Moving average as a 1D array with the same length as `data`.
+        np.ndarray: moving average as a 1D array with the same length as `data`.
     """
 
     window_size: int
@@ -40,7 +37,7 @@ class MovingAverage:
         """Apply the moving average on the data.
 
         Args:
-          data (NDArray): input data as 1D array
+            data (NDArray): input data as 1D array
         """
         window_size = self.window_size
         if window_size > len(data):
