d684e9adb085adf9333a9fd17ad319490a3e2f50
Peter Somhorst <p.somhorst@erasmusmc.nl>
2024-06-25 09:31:11 +0200

    Fix superfluous Path() calls

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,10 +12,10 @@ environment = os.environ.get(
     Path.resolve(Path(__file__).parent.parent),
 )
 data_directory = Path(environment) / "tests" / "test_data"
-draeger_file1 = Path(data_directory) / "Draeger_Test3.bin"
-draeger_file2 = Path(data_directory) / "Draeger_Test.bin"
-timpel_file = Path(data_directory) / "Timpel_test.txt"
-dummy_file = Path(data_directory) / "not_a_file.dummy"
+draeger_file1 = data_directory / "Draeger_Test3.bin"
+draeger_file2 = data_directory / "Draeger_Test.bin"
+timpel_file = data_directory / "Timpel_test.txt"
+dummy_file = data_directory / "not_a_file.dummy"
 
 
 @pytest.fixture(scope="session")
