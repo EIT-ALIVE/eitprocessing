3df6681aa273a97c850218bf1a5b6e55dfb354e4
Peter Somhorst <p.somhorst@erasmusmc.nl>
2024-07-22 14:40:50 +0200

    Fix check amplitude without detected breaths
    If there were no detected breaths left, and you would check the
    amplitudes, this would result in a numpy warning. These are not
    breaking, but can be confusing to the end user.

--- eitprocessing/features/breath_detection.py
+++ eitprocessing/features/breath_detection.py
@@ -337,6 +337,9 @@ class BreathDetection:
         Returns:
             A tuple (peak_indices, valley_indices) with low-amplitude breaths removed.
         """
+        if len(peak_indices) == 0 or len(valley_indices) == 0:
+            return peak_indices, valley_indices
+
         if not self.amplitude_cutoff_fraction:
             return peak_indices, valley_indices
 
