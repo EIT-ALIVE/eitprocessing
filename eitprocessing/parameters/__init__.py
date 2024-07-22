150afded2e9980f7866ad2be21ad3553444336f3
Peter Somhorst <p.somhorst@erasmusmc.nl>
2024-07-22 14:42:42 +0200

    Make ParameterCalculation accept any DataContainer.
    Closes #276.

--- eitprocessing/parameters/__init__.py
+++ eitprocessing/parameters/__init__.py
@@ -2,12 +2,12 @@ from abc import ABC, abstractmethod
 
 import numpy as np
 
-from eitprocessing.datahandling.continuousdata import ContinuousData
+from eitprocessing.datahandling.continuousdata import DataContainer
 
 
 class ParameterCalculation(ABC):
     """Base class for parameter extraction classes."""
 
     @abstractmethod
-    def compute_parameter(self, input_data: ContinuousData) -> np.ndarray:
+    def compute_parameter(self, input_data: DataContainer) -> np.ndarray:
         """Computes the parameter."""
