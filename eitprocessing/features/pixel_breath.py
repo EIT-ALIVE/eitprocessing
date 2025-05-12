0c495b451aa37f0010e6fd947c2d15820329f97a
Peter Somhorst <p.somhorst@erasmusmc.nl>
2025-05-12 16:40:36 +0200

    Fix runtime warning for empty slices

--- eitprocessing/features/pixel_breath.py
+++ eitprocessing/features/pixel_breath.py
@@ -199,7 +199,8 @@ class PixelBreath:
                 cd = np.copy(continuous_data.values)
                 cd -= np.nanmean(cd)
                 pi = np.copy(pixel_impedance[:, row, col])
-                pi -= np.nanmean(pixel_impedance[:, row, col])
+                if not np.all(np.isnan(pi)):
+                    pi -= np.nanmean(pixel_impedance[:, row, col])
 
                 if correct_for_phase_shift:
                     # search for maximum cross correlation within MAX_XCORR_LAG times the average
