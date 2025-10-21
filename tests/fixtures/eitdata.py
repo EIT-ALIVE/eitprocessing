d03ab33f22439c8b57dbb433d9fd0191fd4ba87c
Peter Somhorst <p.somhorst@erasmusmc.nl>
2025-10-21 17:46:38 +0200

    Add pressure pod data fixtures

--- tests/fixtures/eitdata.py
+++ tests/fixtures/eitdata.py
@@ -18,6 +18,16 @@ def draeger_20hz_healthy_volunteer_fixed_rr_path() -> Path:
 
 
 @pytest.fixture
+def draeger_20hz_healthy_volunteer_pressure_pod_path() -> Path:
+    return data_directory / "draeger_20Hz_healthy_volunteer_pressure_pod.bin"
+
+
+@pytest.fixture
+def draeger_50hz_healthy_volunteer_pressure_pod_path() -> Path:
+    return data_directory / "draeger_50Hz_healthy_volunteer_pressure_pod.bin"
+
+
+@pytest.fixture
 def draeger_20hz_healthy_volunteer(draeger_20hz_healthy_volunteer_path: Path) -> Sequence:
     return load_eit_data(
         draeger_20hz_healthy_volunteer_path,
@@ -34,6 +44,26 @@ def draeger_20hz_healthy_volunteer_fixed_rr(draeger_20hz_healthy_volunteer_fixed
         vendor="draeger",
         sample_frequency=20,
         label="draeger_20hz_healthy_volunteer_fixed_rr",
+    )
+
+
+@pytest.fixture
+def draeger_20hz_healthy_volunteer_pressure_pod(draeger_20hz_healthy_volunteer_pressure_pod_path: Path) -> Sequence:
+    return load_eit_data(
+        draeger_20hz_healthy_volunteer_pressure_pod_path,
+        vendor="draeger",
+        sample_frequency=20,
+        label="draeger_20hz_healthy_volunteer_pressure_pod",
+    )
+
+
+@pytest.fixture
+def draeger_50hz_healthy_volunteer_pressure_pod(draeger_50hz_healthy_volunteer_pressure_pod_path: Path) -> Sequence:
+    return load_eit_data(
+        draeger_50hz_healthy_volunteer_pressure_pod_path,
+        vendor="draeger",
+        sample_frequency=50,
+        label="draeger_50hz_healthy_volunteer_pressure_pod",
     )
 
 
