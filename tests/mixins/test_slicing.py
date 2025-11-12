dddfc8e4887b773d58e1d0cc13d398bc93e1bb80
Peter Somhorst <p.somhorst@erasmusmc.nl>
2025-11-12 14:11:01 +0100

    Parametrize single-sequence tests

--- tests/mixins/test_slicing.py
+++ tests/mixins/test_slicing.py
@@ -21,61 +21,62 @@ def test_slicing(sequence: Sequence):
     assert len(sequence) == len(sequence[:cutoff]) + len(sequence[:-cutoff])
 
 
-def test_select_by_time(draeger2: Sequence):
+@pytest.mark.parametrize("sequence", ["timpel_healthy_volunteer_1", "draeger_20hz_healthy_volunteer"], indirect=True)
+def test_select_by_time(sequence: Sequence):
     pytest.skip("selecting by time not finalized yet")
 
     # test illegal
     with pytest.warns(UserWarning, match="No starting or end timepoints was selected"):
-        _ = draeger2.select_by_time()
+        _ = sequence.select_by_time()
     with pytest.warns(UserWarning, match="No starting or end timepoints was selected"):
-        _ = draeger2.select_by_time(None, None)
+        _ = sequence.select_by_time(None, None)
     with pytest.warns(UserWarning, match="No starting or end timepoints was selected"):
-        _ = draeger2.select_by_time(None)
+        _ = sequence.select_by_time(None)
     with pytest.warns(UserWarning, match="No starting or end timepoints was selected"):
-        _ = draeger2.select_by_time(end_time=None)
+        _ = sequence.select_by_time(end_time=None)
 
     # TODO (#82): this function is kinda ugly. Would be nice to refactor it
     # but I am struggling to think of a logical way to loop through.
     ms = 1 / 1000
 
     # test start_time only
-    full_length = len(draeger2)
+    full_length = len(sequence)
     start_slices = [
         # (slice time, expected missing slices if inclusive=True, expected missing slices if inclusive=False)
-        (draeger2.time[22], 22, 22),
-        (draeger2.time[22] - ms, 21, 22),
-        (draeger2.time[22] + ms, 22, 23),
+        (sequence.time[22], 22, 22),
+        (sequence.time[22] - ms, 21, 22),
+        (sequence.time[22] + ms, 22, 23),
     ]
     for test_settings in start_slices:
         print(test_settings)  # noqa: T201
-        sliced_inc = draeger2.select_by_time(start_time=test_settings[0], start_inclusive=True)
+        sliced_inc = sequence.select_by_time(start_time=test_settings[0], start_inclusive=True)
         assert len(sliced_inc) == full_length - test_settings[1]
-        sliced_exc = draeger2.select_by_time(start_time=test_settings[0], start_inclusive=False)
-        assert len(sliced_exc) == len(draeger2) - test_settings[2]
+        sliced_exc = sequence.select_by_time(start_time=test_settings[0], start_inclusive=False)
+        assert len(sliced_exc) == len(sequence) - test_settings[2]
         # test default:
-        assert draeger2.select_by_time(start_time=test_settings[0]) == sliced_inc
+        assert sequence.select_by_time(start_time=test_settings[0]) == sliced_inc
 
     # test end_time only
     end_slices = [
         # (slice time, expected length if inclusive=True, expected length if inclusive=False)
-        (draeger2.time[52], 52, 52),
-        (draeger2.time[52] - ms, 51, 52),
-        (draeger2.time[52] + ms, 52, 53),
+        (sequence.time[52], 52, 52),
+        (sequence.time[52] - ms, 51, 52),
+        (sequence.time[52] + ms, 52, 53),
     ]
     for test_settings in end_slices:
         print(test_settings)  # noqa: T201
-        sliced_inc = draeger2.select_by_time(end_time=test_settings[0], end_inclusive=True)
+        sliced_inc = sequence.select_by_time(end_time=test_settings[0], end_inclusive=True)
         assert len(sliced_inc) == test_settings[1]
-        sliced_exc = draeger2.select_by_time(end_time=test_settings[0], end_inclusive=False)
+        sliced_exc = sequence.select_by_time(end_time=test_settings[0], end_inclusive=False)
         assert len(sliced_exc) == test_settings[2]
         # test default:
-        assert draeger2.select_by_time(end_time=test_settings[0]) == sliced_exc
+        assert sequence.select_by_time(end_time=test_settings[0]) == sliced_exc
 
     # test start_time and end_time
     for start_slicing in start_slices:
         for end_slicing in end_slices:
             # True/True
-            sliced = draeger2.select_by_time(
+            sliced = sequence.select_by_time(
                 start_time=start_slicing[0],
                 end_time=end_slicing[0],
                 start_inclusive=True,
@@ -84,7 +85,7 @@ def test_select_by_time(draeger2: Sequence):
             assert len(sliced) == end_slicing[1] - start_slicing[1]
 
             # False/True
-            sliced = draeger2.select_by_time(
+            sliced = sequence.select_by_time(
                 start_time=start_slicing[0],
                 end_time=end_slicing[0],
                 start_inclusive=False,
@@ -93,7 +94,7 @@ def test_select_by_time(draeger2: Sequence):
             assert len(sliced) == end_slicing[1] - start_slicing[2]
 
             # True/False
-            sliced = draeger2.select_by_time(
+            sliced = sequence.select_by_time(
                 start_time=start_slicing[0],
                 end_time=end_slicing[0],
                 start_inclusive=True,
@@ -102,7 +103,7 @@ def test_select_by_time(draeger2: Sequence):
             assert len(sliced) == end_slicing[2] - start_slicing[1]
 
             # False/False
-            sliced = draeger2.select_by_time(
+            sliced = sequence.select_by_time(
                 start_time=start_slicing[0],
                 end_time=end_slicing[0],
                 start_inclusive=False,
@@ -167,12 +168,13 @@ def test_merging_timing_order(
         _ = Sequence.concatenate(draeger_20hz_healthy_volunteer_fixed_rr, draeger_20hz_healthy_volunteer)
 
 
-def test_concatenate_slicing(timpel1: Sequence):
+@pytest.mark.parametrize("sequence", ["timpel_healthy_volunteer_1"], indirect=True)
+def test_concatenate_slicing(sequence: Sequence):
     # slice and concatenate
     cutoff_point = 100
-    part1 = timpel1[:cutoff_point]
-    part2 = timpel1[cutoff_point:]
-    assert timpel1 == Sequence.concatenate(part1, part2)
+    part1 = sequence[:cutoff_point]
+    part2 = sequence[cutoff_point:]
+    assert sequence == Sequence.concatenate(part1, part2)
 
     # TODO: add tests for
     # - concatenating a third Sequence on top (or two double-sequences), also checking that path attribute is flat list
