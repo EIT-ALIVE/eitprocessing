40e314bf4dcdc3030569ceabdd57ade195a6f813
Peter Somhorst <p.somhorst@erasmusmc.nl>
2025-11-12 15:08:10 +0100

    Fix imports from tests

--- tests/test_parameter_tiv.py
+++ tests/test_parameter_tiv.py
@@ -12,7 +12,7 @@ from eitprocessing.datahandling.eitdata import EITData, Vendor
 from eitprocessing.datahandling.intervaldata import IntervalData
 from eitprocessing.datahandling.sequence import Sequence
 from eitprocessing.datahandling.sparsedata import SparseData
-from eitprocessing.parameters.breath_detection import BreathDetection
+from eitprocessing.features.breath_detection import BreathDetection
 from eitprocessing.parameters.tidal_impedance_variation import TIV
 
 environment = Path(
