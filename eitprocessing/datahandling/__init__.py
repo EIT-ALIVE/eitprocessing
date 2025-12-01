eff38e6e3b70c46b670f63a5c30198d7785f6b21
Peter Somhorst <p.somhorst@erasmusmc.nl>
2025-12-01 15:15:39 +0100

    Add FrozenDataContainer class

--- eitprocessing/datahandling/__init__.py
+++ eitprocessing/datahandling/__init__.py
@@ -1,3 +1,4 @@
+import dataclasses
 from copy import deepcopy
 from dataclasses import dataclass
 
@@ -16,3 +17,26 @@ class DataContainer(Equivalence):
     def deepcopy(self) -> Self:
         """Return a deep copy of the object."""
         return deepcopy(self)
+
+
+@dataclass(eq=False, frozen=True)
+class FrozenDataContainer(Equivalence):
+    """Base class for data container classes."""
+
+    def __bool__(self):
+        return True
+
+    def deepcopy(self) -> Self:
+        """Return a deep copy of the object."""
+        return deepcopy(self)
+
+    def update(self: Self, **kwargs: object) -> Self:
+        """Return a copy of the object with specified fields replaced.
+
+        Args:
+            **kwargs: Fields to replace.
+
+        Returns:
+            A new instance of the object with the specified fields replaced.
+        """
+        return dataclasses.replace(self, **kwargs)
